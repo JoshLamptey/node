56f4e758427912c50b8238cc971b213857ad5814
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-11-21 00:57:07 -1200

    first

---
+++ global.js
@@ -0,0 +1,10 @@
+//setTimeout(()=>{
+//  console.log("this is the 3rd second")
+//    clearInterval(int)
+//},3000)
+
+// const int =setInterval(()=>{
+//    console.log("I am breaking your big geek brain")
+//},1000)
+
+console.log(__dirname,__filename)
