baee4a04041c5015efebdc94b5658c239fa11298
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-11-23 20:54:55 -1200

    first

---
+++ files.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// read files
+//fs.readFile('./docs/blogs.txt',(err,data)=>{
+//    if(err){
+//        console.log(err)
+//    }
+//    console.log(data.toString())
+//})
+
+//to display asynchronousity
+//console.log("last line")
