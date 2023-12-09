b2657fd0ba06d11d24de42da159cb7f84592dee4
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-12-09 03:53:58 -1200

    first

--- files.js
+++ files.js
@@ -12,10 +12,34 @@ const fs = require('fs')
 //console.log("last line")
 
 //  Write files
-fs.writeFile('./docs/blogs2.txt',"Hello Anna",(err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log("file written successfully")
-    }
-})
+//fs.writeFile('./docs/blogs2.txt',"Hello Anna",(err)=>{
+  //  if(err){
+    //    console.log(err)
+   // }else{
+    //    console.log("file written successfully")
+    //}
+//})
+if(!fs.existsSync('./assets')){
+    fs.mkdir("./assets",(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("folder created successfurry")
+        }
+    })
+}else{
+    fs.rmdir('./assets',(err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("folder removed successfury")
+    })
+}
+if(fs.existsSync('./docs/deleteMe.txt')){
+    fs.unlink('./docs/deleteMe.txt',(err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("file removed successfury")
+    })
+}
